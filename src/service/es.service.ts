b142cfa995d9ce12d7de2cf440939f167b842bd9
houxinyue <964973325@qq.com>
2020-12-17 09:41:20 +0800

    d

--- src/service/es.service.ts
+++ src/service/es.service.ts
@@ -36,11 +36,7 @@ export class DbElasticService {
     );
     let axiosData: AxiosRequestConfig = {
       method: method,
-<<<<<<< HEAD
-      url: 'http://localhost:9200/' + urlstr,
-=======
       url: 'http://localhost:9400/' + urlstr,
->>>>>>> b22995e00ba4da8b5a6a744caa8a7b411884876d
       data: body,
       headers: {
         'Content-Type': 'application/json',
