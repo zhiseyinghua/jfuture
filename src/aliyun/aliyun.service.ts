da1fa8bf20feac01b9663f39855ef1cfa3cc7a21
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-05 10:09:49 +0800

    d

--- src/aliyun/aliyun.service.ts
+++ src/aliyun/aliyun.service.ts
@@ -5,21 +5,21 @@ let OSS = require('ali-oss');
 let STS = OSS.STS;
 // TODO: accessKey不能写在代码里
 let sts = new STS({
-  accessKeyId: 'LTAI4Fz4qHdwRBLaVuWAFWJw',
-  accessKeySecret: 'olCiA3RPTAXvEFUiJa9WkVKX77ey5w',
+  accessKeyId: 'LTAI4FzhWZeAwa3obyfrFRet',
+  accessKeySecret: '8Y8m7JfA6GR1MFztAfBZ0euL7O1Qj6',
 });
 @Injectable()
 export class AliyunService {
   public static TSTAllotOSSJurisdiction(): Observable<any> {
     let policy = {
-      Statement: [
+      "Statement": [
         {
-          Action: ['oss:Get*', 'oss:List*'],
-          Effect: 'Allow',
-          Resource: '*',
-        },
-      ],
-      Version: '1',
+            "Action": "oss:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ],
+    "Version": "1"
     };
     console.log('start');
     return from(
