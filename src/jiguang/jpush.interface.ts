9fe52d789b729a30e84b634eefb9db44d6cfc041
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-01-13 09:13:26 +0800

    d

--- src/jiguang/jpush.interface.ts
+++ src/jiguang/jpush.interface.ts
@@ -16,10 +16,44 @@ export interface JPushSMSCodeVerificationRequest {
 }
 
 export interface OrderInterface extends Dbinterface {
-  // key
-  hash: string;
-  range: string;
-  index: string;
   // 地点
   localPlace: any;
+  // 不动产测试   一次性测试
+  type: 'realEstateTest' | 'oneTimeTest';
+  // 预估时间
+  estimatedTime: string;
+  // 面积
+  area: any;
+  // 创建者
+  creator: Dbinterface;
+  // 技术员
+  technician: Dbinterface;
+  // 实际派发时间
+  timeAfterDistribution: Number;
+  // 技术员实际完成时间
+  technicianCompletionTime: Number;
+  // 外业完成时间
+  completionTime: Number;
+  // 内业完成时间
+  insidePagesFinish: Number;
+  // 合同完成时间
+  contractCompleted: Number;
+  // 金额到账时间
+  timeReceiptAmount:Number;
+  // 预估费用
+  estimatedMoney:string;
+  // 实际费用
+  realMoney: string;
+  // 甲方信息
+  ONEinformation:{
+    // 电话
+    phone:string
+    // 邮箱
+    email:string
+    name: string
+  };
+  // 创建任务的人
+  userkey:Dbinterface
+  // 在哪个所属团队发的任务
+  teamkey: Dbinterface
 }
