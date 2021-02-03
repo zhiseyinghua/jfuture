ee9190acaddd6f07a2af606f50cfe0cd0720bbf2
huangwenqiang <huangwenqiang@chain-c.com.com>
2021-02-03 09:29:02 +0800

    d

--- src/figure/figure.service.ts
+++ src/figure/figure.service.ts
@@ -11,49 +11,68 @@ import { Observable, of } from 'rxjs';
 @Injectable()
 export class FigureService {
   public static logger = 'AuthService';
-  public static putOrder(data: PutOrderOne,authkey: Dbinterface) :Observable<any>{
-    let createIdtoken:PutOrderOne = {
+  public static putOrder(
+    data: PutOrderOne,
+    authkey: Dbinterface,
+  ): Observable<any> {
+    let createIdtoken: PutOrderOne = {
       hash: DynamoDBService.computeHash(FIGURE_CONFIG.INDEX),
       range: uuid.v4(),
       index: FIGURE_CONFIG.INDEX,
-      ordername:data.ordername,
+      ordername: data.ordername,
       localPlace: data.localPlace,
       type: data.type,
-      estimatedMoney:data.estimatedMoney,
+      estimatedMoney: data.estimatedMoney,
       estimatedTime: data.estimatedTime,
       area: data.area,
       creatorkey: authkey,
       // 甲方信息
       ONEinformation: data.ONEinformation,
-      timestamp:new Date().valueOf()
+      timestamp: new Date().valueOf(),
     };
     return DbElasticService.executeInEs(
       'PUT',
       FIGURE_CONFIG.INDEX + '/' + FIGURE_CONFIG.DOC + '/' + createIdtoken.range,
-      createIdtoken
+      createIdtoken,
     ).pipe(
       map((result) => {
         // return result;
-        if(result && result._shards && result._shards.successful && result._shards.successful >=1){
-          return createIdtoken
+        if (
+          result &&
+          result._shards &&
+          result._shards.successful &&
+          result._shards.successful >= 1
+        ) {
+          return createIdtoken;
         } else {
-          let err ={
-            code:'000005',
+          let err = {
+            code: '000005',
             meaage: 'server_error',
-          }
-          return err
-        }     
+          };
+          return err;
+        }
       }),
     );
   }
 
   /**
    * 更新甲方信息
-   * @param data 
+   * @param data
    */
-  static firstinformation(data:UpdateFirstinformation) :Observable<any>{
-    console.log(data)
-    return of(12);
+  static firstinformation(data: UpdateFirstinformation): Observable<any> {
+    console.log(data);
+    return DbElasticService.executeInEs(
+      'POST',
+      FIGURE_CONFIG.INDEX +
+        '/' +
+        FIGURE_CONFIG.DOC +
+        '/' +
+        data.range +
+        '/' +
+        FIGURE_CONFIG.UPDATA,
+      {
+        doc: data.ONEinformation,
+      },
+    );
   }
- 
 }
